b01d0470fe844ddf437afb320f2c05e6f034c45e
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-11-01 15:06:24 +0500

    basic app routes are set.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { NotFoundPageComponent, HomeComponent, authRoutes } from './pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  ...authRoutes,
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+  },
+];
