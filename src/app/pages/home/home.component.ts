3c3f465c98d3f7efc994714a54e9bbfa660492a5
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-11-10 20:21:05 +0500

    just simple state to test zone.js

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  count: number = 0;
 
+  clickHandler() {
+    this.count += 1
+  }
 }
