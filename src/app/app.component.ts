cfee2b90e748ddc353a86becee981fde4e45b042
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-11-13 01:34:48 +0500

    toast service is added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { ToastComponent, NavbarComponent } from './shared';
+import { ToastService } from './core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, ToastComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  public toastService: ToastService = inject(ToastService);
+  count = 0;
   title = 'temp';
+
+  handleClick() {
+    this.toastService.add(`Toast ${this.count}`);
+    this.count += 1;
+  }
 }
--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -2,3 +2,4 @@ export * from "./base-http.service";
 export * from "./token.service";
 export * from "./http-error-handling.service";
 export * from "./global-error-handling.service";
+export * from "./toast.service";
