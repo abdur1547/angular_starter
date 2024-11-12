4b5b00df5bc08d2db8c770bbb2e6ae84c6d38bd8
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-11-12 22:37:26 +0500

    login request is added to login component.

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { LoginService } from './login.service';
+import { TokenService } from '../../../core/services/token.service';
 
 @Component({
   selector: 'app-login',
@@ -10,14 +11,24 @@ import { LoginService } from './login.service';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  private loginService:LoginService = inject(LoginService);
-  
-  loginForm:FormGroup = new FormGroup({
+  private loginService: LoginService = inject(LoginService);
+  private tokenService: TokenService = inject(TokenService);
+
+  loginForm: FormGroup = new FormGroup({
     email: new FormControl(''),
     password: new FormControl('')
   });
 
   login() {
-    this.loginService.login(this.loginForm.value);
+    this.loginService.login(this.loginForm.value)
+      .subscribe(tokens => this.tokenService.saveTokens(tokens.data.access_token, tokens.data.refresh_token))
   };
+
+  getTokens() {
+    console.log(this.tokenService.getAccessToken());
+  }
+
+  clearTokens() {
+    console.log(this.tokenService.clearTokens());
+  }
 }
--- src/app/pages/auth/login/types.ts
+++ src/app/pages/auth/login/types.ts
@@ -2,3 +2,11 @@ export type LoginCredentials = {
     email: string;
     password: string;
 };
+
+export type LoginResponce = {
+    success: boolean;
+    data: {
+        access_token: string;
+        refresh_token: string;
+    }
+};
