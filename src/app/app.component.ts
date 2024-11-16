13ba9ad36dc03f7acccc1bd0d3217b296d585b2b
Abdur Rahman <khalil.ahmad1547@gmail.com>
2024-11-16 15:20:35 +0500

    auto refresh logic is added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,30 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastComponent, NavbarComponent } from './shared';
-import { ToastService } from './core';
+import { ToastService, TokenService } from './core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent, ToastComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  private tokenService: TokenService = inject(TokenService);
   public toastService: ToastService = inject(ToastService);
   count = 0;
   title = 'temp';
+
+  ngOnInit() {
+    console.log('App OnInit');
+    this.tokenService.autoRefreshAccessToken();
+  }
+
+  ngOnDestroy() {
+    console.log('App OnDestroy');
+    this.tokenService.clearTimer();
+  }
 
   handleClick() {
     this.toastService.add(`Toast ${this.count}`);
